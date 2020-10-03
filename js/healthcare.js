13a44569fe07b696a41f40e7658bb0b9cbd23ec6
staringElf <msoffice37@gmail.com>
2020-10-03 17:30:29 +0545

    automate the product cards

--- js/healthcare.js
+++ js/healthcare.js
@@ -1,28 +1,68 @@
-const products = [
-    {
-        title: 'Mask 1',
-        src: '../Images/3.jpg',
-        desc: 'Nothing'
-    },
-]
+const productsSection = document.querySelector('.our-products');
 
-const ul = document.querySelector('ul');
-ul.classList.add('products');
+const categories = [
+    [
+        {
+            name: 'Mask',
+            desc: 'Description',
+            type: 'mask',
+        },
+        
+        {
+            name: 'Mask',
+            desc: 'Description',
+            type: 'mask',
+        },
+        
+        {
+            name: 'Mask',
+            desc: 'Description',
+            type: 'mask',
+        }
+    ]
+];
 
-products.forEach(product => {
-    ul.innerHTML = `<li class="product">
-        <div class="product__body">
+const ids = document.querySelectorAll('.side__link');
+
+categories.forEach((category, index)  => {
+    const categoryUl = document.createElement('ul');
+    categoryUl.id = ids[index].getAttribute('href').substring(1);
+    category.forEach((product, index) => {
+        const productLi = document.createElement('li');
+        productLi.innerHTML = `<div class="product__body">
             <h3 class="product__title">
-                <a href="">${product.title}</a>
+                <a href="">${product.name}-${index+1}</a>
             </h3>
             <p>${product.desc}</p>
-            <small>By Great Products(TM)</small>
+            <small>By ApexIndia</small>
         </div>
         <div class="product__image">
             <a>
-                <img class="img" src="${product.src}" alt="${product.desc}">
+                <img class="img" src="products/${product.type}-${index+1}.jpg" alt="Description of the great product's appearance">
             </a>
-            </div>
-        </li>
-        `
+            </div>`
+        productLi.classList.add('product');
+        categoryUl.appendChild(productLi);
+    })
+    categoryUl.classList.add('products');
+    productsSection.appendChild(categoryUl);
 })
+
+
+/*
+<ul class="products products--masks" id="masks">
+<li class="product">
+    <div class="product__body">
+        <h3 class="product__title">
+            <a href="">A great product</a>
+        </h3>
+        <p>Description of the great product</p>
+        <small>By Great Products(TM)</small>
+    </div>
+    <div class="product__image">
+        <a>
+            <img class="img" src="Images/b.jpg" alt="Description of the great product's appearance">
+        </a>
+        </div>
+</li>
+*/
