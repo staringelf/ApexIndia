84478f409c17dc9fb270b80c33d31887c033b9d0
Manish <msoffice37@gmail.com>
2020-10-12 01:45:18 -0700

    make the popup dynamic

--- js/print.js
+++ js/print.js
@@ -1,16 +1,25 @@
 const swiperTriggers = document.querySelectorAll('.swiper-trigger');
 const modal = document.querySelector('.modal');
 const closeModal = modal.querySelector('.close_card');
+const swiperWrapper = document.querySelector('.swiper-wrapper');
 let swiper;
-
+let category = 'box';
 
 swiperTriggers.forEach(trigger => trigger.addEventListener('click', function(e){
     target = e.target;
     const productLi = target.closest('.product');
+    console.log(productLi);
+    category = productLi.dataset.category;
+    console.log(category);
     modal.classList.add('appear');
-    // console.log(productLi); 
-    
-    swiper = new Swiper('.swiper-container', {
+      for(let x = 0; x < 3; x++){
+        const swiperSlide = document.createElement('div');
+        swiperSlide.classList.add('swiper-slide');
+        swiperSlide.innerHTML = `<img src="products/design/${category}-${x+1}.jpg" alt="" >`;
+        swiperWrapper.appendChild(swiperSlide);
+      }
+
+      swiper = new Swiper('.swiper-container', {
         loop: true,
         grabCursor: true,
         navigation: {
@@ -27,4 +36,5 @@ swiperTriggers.forEach(trigger => trigger.addEventListener('click', function(e){
 closeModal.addEventListener('click', function () {
     modal.classList.remove('appear');
     swiper.destroy();
+    swiperWrapper.textContent = '';
 })
